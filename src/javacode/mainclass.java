840cc7f94c3e567c3c2b48d5f756426ba792c24b
Akshika Tyagi <tyagiakshika13@gmail.com>
2025-07-30 13:58:05 +0530

    new codes

---
+++ src/javacode/mainclass.java
@@ -0,0 +1,17 @@
+package javacode;
+import java.util.*;
+
+public class mainclass {
+	public static void main(String arg[]) {
+		int a,b;
+		Scanner sc = new Scanner(System.in);
+		abc obj = new abc();
+		System.out.println("enter two no.");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		obj.add(a,b);	
+		}
+	}
+		// TODO Auto-generated constructor stub
+
+
---
+++ src/javacode/overloading.java
@@ -0,0 +1,29 @@
+package javacode;
+import java.util.*;
+
+public class overloading {
+	int a,b;
+	public void add() {
+	this.a=5;
+	this.b=10;
+	System.out.println("sum without input:" +(this.a+this.b));
+	}
+	
+	public void add(int a,int b) {
+		this.a=a;
+		this.b=b;
+		System.out.println("sum with imput:" +(this.a+this.b));
+		
+	}
+	
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		overloading obj = new overloading();
+		obj.add();
+		System.out.println("enter the 1st number:");
+		int num1 = sc.nextInt();
+		System.out.println("enter the 2nd number:");
+		int num2 = sc.nextInt();
+		
+	}
+}
